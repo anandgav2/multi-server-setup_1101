ba6832c5e9f2531b17f624cac88f18ea1e8af202
Tanya Patel <38834492+tanyaP20@users.noreply.github.com>
2023-10-12 18:47:54 +0530

    added count variable

--- scripts/terraform/variables.tf
+++ scripts/terraform/variables.tf
@@ -6,3 +6,9 @@ variable "instance_type" {
   description = "Type of Instance"
   default     = "r6i.2xlarge"
 }
+
+variable "server_cnt" {
+  description = "This is a variable of type number"
+  type        = number
+  default     = 1
+}
