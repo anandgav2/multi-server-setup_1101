5fb4625a8b04f62109f34711f57087e04a40c02f
skuad_tanyap <tanya.patel@skuad.io>
2023-11-30 09:30:54 +0530

    open all ports

--- scripts/terraform/vpc.tf
+++ scripts/terraform/vpc.tf
@@ -4,14 +4,11 @@ resource "aws_security_group" "cip-multi-internal" {
   name        = "cip-multi-internal"
   description = "Security group for internal communication"
 
-  dynamic "ingress" {
-    for_each = aws_instance.cip
-    content {
-      from_port   = 0
-      to_port     = 65535
-      protocol    = "tcp"
-      cidr_blocks = ["${ingress.value.public_ip}/32"]
-    }
+  "ingress" {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -28,55 +25,3 @@ resource "aws_security_group" "cip-multi-internal" {
     cidr_blocks = ["0.0.0.0/0"]  
   }
 }
-
-# Security Group for External Communication 
-resource "aws_security_group" "cip-multi-external" {
-   name        = "cip-multi-external"
-   description = "Security group for external communication"
-
-  ingress {
-    from_port   = 22  # SSH port
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Restrict this to your IP range for better security
-  }
-
-  ingress {
-    from_port   = 6761  # Your desired port # AG
-    to_port     = 6761
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Restrict this to your IP range for better security
-  }
-
-  ingress {
-    from_port   = 6961  # Your desired port # Cafe
-    to_port     = 6961
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Restrict this to your IP range for better security
-  }
-
-  ingress {
-    from_port   = 6461  # Your desired port # CC
-    to_port     = 6461
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Restrict this to your IP range for better security
-  }
-
-  ingress {
-    from_port   = 32400  # Your desired port  # CIPUI
-    to_port     = 32400
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Restrict this to your IP range for better security
-  }
-
-  ingress {
-    from_port   = 29000  # Your desired port #A360
-    to_port     = 29092
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Restrict this to your IP range for better security
-  }
-
-}
-
-
-
